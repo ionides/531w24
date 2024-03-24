c35f7eaec720f12da53bb63f55c8622aaf62f3fd
LiangqiTang <144564789+LiangqiTang@users.noreply.github.com>
2024-03-23 22:24:01 -0400

    1

---
+++ helloF.f
@@ -0,0 +1,11 @@
+
+      subroutine hello
+      external dblepr
+      call dblepr(" ",-1,0,0)
+      call dblepr(" ",-1,0,0)
+      call dblepr(" ",-1,0,0)
+      call dblepr("Hello from native FORTRAN!",-1,0,0)
+      call dblepr(" ",-1,0,0)
+      call dblepr(" ",-1,0,0)
+      call dblepr(" ",-1,0,0)
+      end
